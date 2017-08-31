7251d8022118b8c5850f680a549682cf32dc75c7
Dan Gershony <dan.gershony@gmail.com>
2017-08-31 19:25:28 +0100

    WithdrawTransaction

---
+++ NBitcoin/SpvProof.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NBitcoin
+{
+	public class WithdrawTransaction :IBitcoinSerializable
+	{
+		public uint256 ParentGenesis;
+
+		public SpvProof SpvProof;
+
+
+		public void ReadWrite(BitcoinStream stream)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+    public class SpvProof
+    {
+	    private List<BlockHeader> Headers;
+
+    }
+}
